2007ac0d8f07c30b17c73531a43eec57eaf633c7
VLuca997 <kulertoast997@gmail.com>
2023-07-06 01:29:33 +0200

    ADD Axios

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const { createApp } = Vue;
-const app = CreateApp({
+const app = createApp({
     data(){
         return{
             emails: [],
@@ -7,6 +7,22 @@ const app = CreateApp({
         }
     },
     methods: {
-        
+        generateEmails(){
+            this.loading = true;
+            const requests = [];
+            for(let i=0 ;i < 10; i++ ){
+                requests.push(axios.get('https://flynn.boolean.careers/exercises/api/random/mail'));
+            };
+            axios
+            .all(requests)
+            .then(responses => {
+            this.emails = responses.map(response => response.data.response);
+            this.loading = false;
+            })
+            .catch(error => {
+            console.error(error);
+            this.loading = false;
+            });
+        }
     }
 });app.mount('#app')
